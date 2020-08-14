8b6a572fed8ad4dcc46b6794b138dd5252f8e3b8
lishiwei <lishiwei@dxy.cn>
2020-08-14 15:47:48 +0800

    <feat>:(检验工具)check.py/(时间工具)time_packages.py

---
+++ check.py
@@ -0,0 +1,268 @@
+import sys
+import time
+import json
+import random
+import requests
+import functools
+
+from copy import deepcopy
+from inspect import signature
+
+
+class Check(object):
+    """
+    检查下载后的html
+    """
+
+    @staticmethod
+    def pos_html(h, pos=None):
+        """
+        检查是否h中是否包含pos
+        :params: h: 下载后的html
+        :params: pos: 是否包含
+        :return: <bool> true/false
+        """
+        if Check.is_json(h):
+            return True
+        if not pos:
+            pos = ['<html>']
+        for i in pos:
+            if str(i).strip() in h:
+                return True
+        return False
+
+    @staticmethod
+    def is_json(my_json):
+        """
+        判断是否是json类型
+        """
+        try:
+            json.loads(my_json)
+        except ValueError:
+            return False
+        return True
+
+    @staticmethod
+    def is_dict_value_null(target_dict, pop_keys=None):
+        """
+        判断target_dict是否均为空
+        False: 字典值中有数据
+        True: 字典值中无数据
+        :params: target_dict: <dict> 目标待处理字典
+        :params: pop_keys: <list/str> 需要排除处理的key
+        """
+        __target_dict = deepcopy(target_dict)
+        pop_keys_list = list()
+        if isinstance(pop_keys, str):
+            pop_keys_list.append(pop_keys)
+        elif isinstance(pop_keys, list):
+            pop_keys_list = pop_keys
+        if pop_keys_list:
+            for i in pop_keys_list:
+                __target_dict.pop(i, '')
+        for sin_va in __target_dict.values():
+            if sin_va:
+                return False
+        return True
+
+    @staticmethod
+    def is_request_valid(url, timeout=30):
+        """
+        检测url是否可以请求通
+        """
+        try:
+            res = requests.get(url, timeout=timeout)
+        except requests.exceptions.ConnectTimeout:
+            print('[IS_REQUEST_VALID]url:%s error: timeout' % url)
+            return False
+        except Exception as err:
+            print('[IS_REQUEST_VALID]url:%s error:%s' % (url, err))
+            return False
+        res_status = res.status_code
+        if res_status == 200:
+            return True
+        return False
+
+    @staticmethod
+    def is_compress_dict(target_dict):
+        """
+        是否将正常字典压缩（将字典中为空的值去除掉）
+        """
+        assert isinstance(target_dict, dict), 'param must be dict!'
+        new_dict = dict()
+        for sin_k, sin_v in target_dict.items():
+            if not sin_v:
+                continue
+            new_dict[sin_k] = sin_v
+        return new_dict
+
+    @staticmethod
+    def is_contains_chinese(strs):
+        """
+        检验字符串中是否含有中文字符
+        """
+        for _char in strs:
+            if '\u4e00' <= _char <= '\u9fa5':
+                return True
+        return False
+
+    @staticmethod
+    def is_df_contains_value(df, column_name,
+                             value, is_judge_origin=False):
+        """
+        检查df中的column_name列中是否包含value
+        :param df: dataFrame
+        :param column_name: df中的某列名
+        :param value: <str> 某字符串（用来判断是否在df中）
+        :param is_judge_origin: <bool> 是否需要判断初始df(和业务相关)
+                  is_judge_origin:True 且初始df为空，则判定存在
+        :return <bool> True:存在 False: 不存在
+        """
+        if is_judge_origin and df.empty:
+            return True
+        if column_name not in df.columns:
+            raise TypeError('column_name not in df!')
+        df_res = df.loc[df[column_name] == value]
+        if df_res.empty:
+            return False
+        return True
+
+
+class Bunch(dict):
+    """
+    Container object for datasets
+    Dictionary-like object that exposes its keys as attributes.
+
+    >>> b = Bunch(a=1, b=2)
+    >>> b['b']
+    2
+    >>> b.b
+    2
+    >>> b.a = 3
+    >>> b['a']
+    3
+    >>> b.c = 6
+    >>> b['c']
+    6
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __dir__(self):
+        return self.keys()
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+
+def check_fun_error(func):
+    """
+    检查函数的报错
+    如果函数自身报错，则重新运行函数本身
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            fun_res = func(*args, **kwargs)
+        except Exception:
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print('[ERROR] check_fun restart!, error info:%s, value:%s' % (
+                exc_type, exc_value))
+            time.sleep(random.uniform(2, 5))
+            fun_res = func(*args, **kwargs)
+        return fun_res
+
+    return wrapper
+
+
+def check_fun_return(func):
+    """
+    检查函数值是否报错更新返回值
+    如果函数自身报错，则返回False,否则正常返回
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            fun_res = func(*args, **kwargs)
+        except Exception:
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            print('check_fun return False!, error info:%s, value:%s' % (
+                exc_type, exc_value))
+            fun_res = False
+        return fun_res
+
+    return wrapper
+
+
+def param_type_assert(*args, **kargs):
+    """
+    函数参数验证器
+    :param args:
+    :param kargs:
+    :return:
+    case:
+    # @param_type_assert(d=int)
+    # @param_type_assert(int, str)
+    @param_type_assert(c=list, a=int)
+    def demo(a, b, c, d=1):
+        print(a, b, c, d)
+    """
+
+    def decorator(func):
+        assert_func = signature(func)
+        assert_param_dict = assert_func.bind_partial(*args, **kargs).arguments
+
+        @functools.wraps(func)
+        def wrapper(*args, **kargs):
+            for param_name, param_value in assert_func.bind(*args, **kargs).arguments.items():
+                if param_name in assert_param_dict:
+                    if not isinstance(param_value, assert_param_dict[param_name]):
+                        raise TypeError('[CHECK_PARAM] %s must be %s' % (param_name, assert_param_dict[param_name]))
+            return func(*args, **kargs)
+
+        return wrapper
+
+    return decorator
+
+
+def check_seed_expression_type(seeds_info, expre_type='save'):
+    '''
+    为了兼容种子存储结构的变化而设立的方法, 待后续所有业务都使用了种子模板后可以去掉内层的 if 判断
+    :param seeds_info: 种子
+    :param expre_type: 是save保存数据, 还是 update更新过程表数据
+    :return: 返回见具体
+    '''
+    storage_info = seeds_info.get('storage_info')
+    store_expression = storage_info['expression']
+    if expre_type == 'save':
+        if isinstance(store_expression, dict):
+            store_info_expression = storage_info['expression']
+        else:
+            store_info_expression = storage_info['expression'][0]
+        return store_info_expression
+    if expre_type == 'update':
+        if isinstance(store_expression, dict):
+            process_table_update = storage_info['list_expression']
+        else:
+            process_table_update = storage_info['expression'][1]
+        return process_table_update
+
+
+if __name__ == '__main__':
+    # @param_type_assert(d=int)
+    # @param_type_assert(int, str)
+    @param_type_assert(c=list, a=int)
+    def demo(a, b, c, d=1):
+        print(a, b, c, d)
+
+
+    demo(1, '2', [1, 2])
---
+++ time_package.py
@@ -0,0 +1,264 @@
+import time
+import datetime
+import functools
+
+
+def date_time(offset_day=0, special_date=False, time_type='str'):
+    """
+    :param offset_day: <int> Days in advance
+    :param special_date:<str>  String day，若给定，则在此基础上推移
+    :param time_type:<str>  str 返回str类型时间；
+                            dt 返回datetime.datetime；
+    :return:
+    :example:
+      date_time()
+      date_time(1)
+      date_time(1, special_date='2019-01-02')
+      date_time(1, special_date='2019-01-02 00:00:00')
+    """
+
+    def _change_time_type(time_element, ft='%Y-%m-%d'):
+        """
+        切换时间格式
+        :param time_element:
+        :param ft:
+        :return:
+        """
+        _res_date = time_element.strftime(ft)
+        if time_type == 'dt':
+            _res_date = time_element
+        return _res_date
+
+    assert time_type in ('str', 'dt'), TypeError('time type dont supported, exp:"str"or"dt"')
+    today = datetime.date.today()
+    fmt = '%Y-%m-%d'
+    if special_date:
+        assert isinstance(special_date, str), TypeError('time_type type error!')
+        if len(special_date) > 10:
+            fmt = '%Y-%m-%d %H:%M:%S'
+        today = datetime.datetime.strptime(special_date, fmt)
+    offday = datetime.timedelta(offset_day)
+    target_day = today - offday
+    return _change_time_type(target_day, fmt)
+
+
+def current_time(time_format='%Y-%m-%d %H:%M:%S'):
+    """
+    返回格式化后的当前时间
+    """
+    current_time = str(datetime.datetime.now().strftime(time_format))
+    return current_time
+
+
+def get_first_day_of_month(ts=None):
+    """
+    # 获取本月第一天
+    :param ts:
+    :return: 本月第一天时间戳
+    """
+    if ts:
+        day = datetime.datetime.fromtimestamp(ts)
+    else:
+        day = datetime.date.today()
+    day_of_month = day - datetime.timedelta(days=day.day - 1)
+    dt = datetime.datetime(day_of_month.year, day_of_month.month, day_of_month.day, 0, 0, 0)
+    t = dt.timetuple()
+    time_stamp = int(time.mktime(t))
+    return time_stamp
+
+
+def get_time_delta(time_type='hours', time_delta=1.0, is_all=False):
+    """
+    获取时间差，支持小时/天/分钟/秒
+    :param: time_type: 时间类型 day/hour/minute/second
+    :param: time_delta: 时间间隔（按照小时/单位）
+    :param: is_all<bool> 是否拼接字段
+    """
+    current_time = datetime.datetime.now()
+    if time_type == 'days':
+        _time_res = current_time - datetime.timedelta(days=time_delta / 24)
+        time_res = _time_res.strftime("%Y-%m-%d")
+    elif time_type == 'hours':
+        _time_res = current_time - datetime.timedelta(hours=time_delta)
+        time_res = _time_res.strftime("%Y-%m-%d %H")
+    elif time_type == 'minutes':
+        _time_res = current_time - datetime.timedelta(minutes=time_delta * 60)
+        time_res = _time_res.strftime("%Y-%m-%d %H:%M")
+    elif time_type == 'seconds':
+        _time_res = current_time - datetime.timedelta(seconds=time_delta * 60 * 60)
+        time_res = _time_res.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        raise TypeError('time_type type error!')
+    if is_all:
+        time_res = _time_res.strftime("%Y-%m-%d %H:%M:%S")
+    return time_res
+
+
+def change_to_timestamp(str_time, ft='%Y-%m-%d'):
+    """
+    str时间转timestamp
+    :param str_time: <str> strtime
+    :param ft: 格式
+    :return:
+    """
+    time_array = time.strptime(str_time, ft)
+    time_stamp = int(time.mktime(time_array))
+    return time_stamp
+
+
+def get_current_timestamp(is_second_or_millisecond=True):
+    """
+    获取当前时间戳
+    :params: is_second_or_millisecond: <bool>
+    :return: current timestamp
+    """
+    if is_second_or_millisecond:
+        current_timestamp = int(time.time())
+    else:
+        current_timestamp = int(round(time.time() * 1000))
+    return current_timestamp
+
+
+def time_section(type='year', return_type='int'):
+    """
+    time section dealer
+    input:year output:2018
+    input:month output:05/5
+    input:second output:05/5
+    :param type: <str> year/month/day/hour/minute/second/week
+    :param return_type: <str> final result type is int or str, 默认返回 int
+    :return:
+    """
+    if type == 'year':
+        target = datetime.datetime.now().date().year
+    elif type == 'month':
+        target = datetime.datetime.now().date().month
+    elif type == 'day':
+        target = datetime.datetime.now().date().day
+    elif type == 'week':
+        target = datetime.datetime.now().weekday() + 1
+    elif type == 'hour':
+        target = datetime.datetime.now().hour
+    elif type == 'minute':
+        target = datetime.datetime.now().minute
+    elif type == 'second':
+        target = datetime.datetime.now().second
+    else:
+        raise Exception('type error')
+    if return_type == 'str':
+        return str(target)
+    return target
+
+
+def get_week_today(ts=None):
+    """
+    获得今天是周几0-6对应周日到周六
+    """
+    a = time.localtime()
+    if ts:
+        a = time.localtime(ts)
+    return int(time.strftime('%w', a))
+
+
+def format_str_date(pend_date, reverse=False):
+    """
+    字符串时间 和 时间格式相互转换
+    :param pend_date:<str>shijian
+    :param reverse:<bool> 默认false即字符串时间转时间格式，
+                              ture即时间格式转str
+    :return:
+    """
+    assert pend_date and isinstance(pend_date, (str, datetime.datetime, datetime.date)), \
+        TypeError('time_type type error!')
+    ft = '%Y-%m-%d %H:%M:%S'
+    if len(str(pend_date)) < 11:
+        ft = '%Y-%m-%d'
+    if isinstance(pend_date, (datetime.datetime, datetime.date)):
+        if not reverse:
+            return pend_date
+        return pend_date.strftime(ft)
+    # 2020-06-09 20:55:59.145
+    str_date_time = pend_date.split('.')[0]
+    res_date_time = datetime.datetime.strptime(str_date_time, ft)
+    return res_date_time
+
+
+def get_date_list(start, end, fmt="%Y-%m-%d %H:%M:%S", is_reverse=False):
+    """
+    获取日期区间列表
+    :param start: 开始日期
+    :param end: 结束日期
+    :return:
+    """
+    start = format_str_date(start)
+    end = format_str_date(end)
+    data = []
+    day = datetime.timedelta(days=1)
+    for i in range((end - start).days):
+        re = start + day * i
+        data.append(re.strftime(fmt))
+    data.append(end.strftime(fmt))
+    if is_reverse:
+        data.reverse()
+    return data
+
+
+def check_func_time(func):
+    """
+    判断函数运行时间
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = get_current_timestamp(is_second_or_millisecond=False)
+        func_state = func(*args, **kwargs)
+        end_time = get_current_timestamp(is_second_or_millisecond=False)
+        delta_time = int(end_time - start_time) / 1000
+        print('%s use %ss' % (func.__name__, delta_time))
+        return func_state
+
+    return wrapper
+
+
+def get_month_first_last(now_day: datetime.datetime = datetime.datetime.now(), return_type: str = 'str') -> tuple:
+    '''
+    返回上个月第一天和最后一天的日期时间
+    :param now_day: 默认当前时间 <datetime.datetime> 数据类型
+    :param return_type: 返回的类型, 是 str 还是 datetime.datetime
+    :return: 单参数返回<tuple>, 双参数各自返回<return_type所传类型>, date_from 上个月第一天, date_to 上个月最后一天
+      eg: 当前是 7 月份, 则返回2020-06-01 00:00:00, 2020-06-30 23:59:59
+    '''
+    dayscount = datetime.timedelta(days=now_day.day)
+    dayto = now_day - dayscount
+    date_first = datetime.datetime(dayto.year, dayto.month, 1, 0, 0, 0)
+    date_last = datetime.datetime(dayto.year, dayto.month, dayto.day, 23, 59, 59)
+    assert (return_type in ['str', 'datetime']), '只支持返回 str 或 datetime.datetime 格式'
+    if return_type == 'str':
+        return str(date_first), str(date_last)
+    return date_first, date_last
+
+
+if __name__ == '__main__':
+    # print(change_to_timestamp())
+    # print(current_time())
+    # print(current_time(time_format='%Y-%m-%d'))
+    # print(get_time_delta())
+    # print(6 - get_week_today())
+    # print(get_date_list('2019-1-1 10:00:00', '2019-10-1', fmt='%d/%m/%Y'))
+    # a = change_to_timestamp(date_time(-1), ft='%Y-%m-%d')
+    # print(a)
+    print(date_time(-1))
+    # print(get_time_delta(time_type='hours', time_delta=1, is_all=True))
+    # print(date_time(1, special_date='2019-01-02 00:00:00'))
+    # print(get_current_timestamp())
+    # print(get_current_timestamp(is_second_or_millisecond=False))
+    # print(time_section('day'))
+    # print(time_section('hour'))
+    # print(current_time())
+    # print(current_time('%Y-%m-%d %H:%M'))
+    # print(get_time_delta(time_type='days', time_delta=45))
+    # print(get_time_delta(time_type='hours', time_delta=2))
+    # print(get_time_delta(time_type='minutes', time_delta=1/6))
+    # print(get_time_delta(time_type='seconds', time_delta=1))
+    # print(get_time_delta(time_type='hours', time_delta=1, is_all=True))
+    # print(get_time_delta(time_type='minutes', time_delta=1, is_all=True))
